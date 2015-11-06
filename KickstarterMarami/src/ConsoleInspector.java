b65f14540b6d58cef7e02a3fcfcbeed3b7606b90
Marina Dobritsa <maramister@gmail.com>
2015-11-06 13:20:20 +0200

    changed name of folder

---
+++ KickstarterMarami/src/ConsoleInspector.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConsoleInspector {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static int getInt() {
+		int a;		
+		a = sc.nextInt();		
+		return a;
+	}		
+	
+	public static void close() {
+		sc.close();
+	}
+}
